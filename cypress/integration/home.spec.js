ae183bb96212842c81ae8859c031ad5769de7367
Ralph Unrau <xralphunrau@gmail.com>
2022-05-10 23:14:12 +0000

    Add first two cypress tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+describe("Navigation", () => {
+
+  it("should visit root", () => {
+    cy.visit('/');
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 3);
+  });
+});
