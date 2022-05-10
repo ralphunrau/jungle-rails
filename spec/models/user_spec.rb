d7534bdf6c08b40bdabe6bb88323630d25014324
Ralph Unrau <xralphunrau@gmail.com>
2022-05-10 01:26:43 +0000

    Add tests for incorrent for incorrect form fillouts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,32 +3,67 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
 
-    context '' do
-      it '' do
-        expect().to eq()
+    context 'with all necessary fields' do
+      it 'creates a new user' do
+        @user = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password: 'pass',
+          password_confirmation: 'pass'
+        )
+        expect(@user.errors.full_messages).to eq([])
       end
     end
 
-    context '' do
-      it '' do
-        expect().to eq()
+    context 'no password field' do
+      it 'should error out' do
+        @user = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password_confirmation: 'pass'
+        )
+        expect(@user.errors.full_messages).to eql(["Password can't be blank"])
       end
     end
-    context '' do
-      it '' do
-        expect().to eq()
+    context 'no password confirmation field' do
+      it 'should error out' do
+        @user = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password: 'pass'
+        )
+        expect(@user.errors.full_messages).to eql(["Password confirmation can't be blank"])
       end
     end
 
-    context '' do
-      it '' do
-        expect().to eq()
+    context 'non-matching password and confirmation fields' do
+      it 'returns an error' do
+        @user = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password: 'pass',
+          password_confirmation: 'incorrect_pass'
+        )
+        expect(@user.errors.full_messages).to eql(["Password confirmation doesn't match Password"])
       end
     end
 
-    context '' do
-      it '' do
-        expect().to eq()
+    context 'adding user who already exists into database' do
+      it 'returns an error for the second user' do
+        @user1 = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password: 'pass',
+          password_confirmation: 'pass'
+        )
+        @user2 = User.create(
+          name: 'Test',
+          email: 'tester@example.com',
+          password: 'pass',
+          password_confirmation: 'pass'
+        )
+        expect(@user1.errors.full_messages).to eq([])
+        expect(@user2.errors.full_messages).to eq(["Email has already been taken"])
       end
     end
   end
