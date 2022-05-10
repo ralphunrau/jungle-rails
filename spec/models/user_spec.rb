8027e99496f0b0b27d1e84724498c3285cb49809
Ralph Unrau <xralphunrau@gmail.com>
2022-05-10 01:14:52 +0000

    Add user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    context '' do
+      it '' do
+        expect().to eq()
+      end
+    end
+
+    context '' do
+      it '' do
+        expect().to eq()
+      end
+    end
+    context '' do
+      it '' do
+        expect().to eq()
+      end
+    end
+
+    context '' do
+      it '' do
+        expect().to eq()
+      end
+    end
+
+    context '' do
+      it '' do
+        expect().to eq()
+      end
+    end
+  end
+end
