66a05472152d9588bd7517470c0a949ee24a1f9e
Ralph Unrau <xralphunrau@gmail.com>
2022-05-10 01:28:28 +0000

    Add validation for password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   
   validates :password_confirmation, presence: true
   validates :email, :uniqueness => {:case_sensitive => false}
-
+  validates_length_of :password, :minimum => 8
 end
